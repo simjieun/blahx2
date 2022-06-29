015681e8a658d154d1ed3233a283df6cbb27bdd5
simjieun <simjiuen@gmail.com>
2022-06-29 22:39:44 +0900

    사용자 모델 추가

--- pages/api/members.add.ts
+++ pages/api/members.add.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import FirebaseAdmin from '@/models/firebase_admin';
+import MemberModel from '@/models/member/member.model';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { uid, email, displayName, photoURL } = req.body;
@@ -10,18 +10,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(400).json({ result: false, message: 'uid가 누락되었습니다' });
   }
 
-  try {
-    const addResult = await FirebaseAdmin.getInstance()
-      .Firebase.collection('members')
-      .add({
-        uid,
-        email: email ?? '',
-        displayName: displayName ?? '',
-        photoURL: photoURL ?? '',
-      });
-    return res.status(200).json({ result: true, id: addResult });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ result: false });
+  if (email === undefined || email === null) {
+    res.status(400).json({ result: false, message: 'email이 누락되었습니다' });
   }
+
+  const addResult = await MemberModel.add({ uid, email, displayName, photoURL });
+
+  if (addResult.result === true) {
+    return res.status(200).json(addResult);
+  }
+
+  res.status(500).json(addResult);
 }
